d9e9485183c10007aa3d856afe4179073aaaa6e0
Florent Espanet <florent.esp@gmail.com>
2014-04-17 10:39:09 +0200

    Blur an image.  Add usage.  Add examples.

--- pycut/pycut.py
+++ pycut/pycut.py
@@ -1,5 +1,23 @@
+from sys import argv, stderr
+from PIL import Image, ImageFilter
+
+def usage():
+    stderr.write(("Usage : pycut IMAGE [OPTIONS]...\n"
+                  "Try 'pycut --help' for more information.\n"))
+    exit(1)
+    
+
+def parse_args():
+    if len(argv) != 2:
+        usage()
+    else:
+        return argv[1]
+
 def pycut():
-    return 0
+    image_name = parse_args()
+    image = Image.open(image_name)
+    blur = image.filter(ImageFilter.GaussianBlur(75))
+    blur.save("blured-{}".format(image_name))
 
 if __name__ == "__main__":
     pycut()
